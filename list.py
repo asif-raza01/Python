6e252a34cd112930edec5d09ef8e9feab923daee
asif <155326429+asif-raza01@users.noreply.github.com>
2026-03-03 23:22:25 +0530

    changed list.py

---
+++ list.py
@@ -0,0 +1,4 @@
+nums=[1,2,3,4,5] #This is a list of integers
+print(nums) #This will print the entire list
+print(nums[0]) #This will print the first element of the list, which is 1
+print(nums[2]) #This will print the third element of the list, which is 3
