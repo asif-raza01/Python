4048b4b875e32320198f0343ff257cf5e4301248
asif-raza01 <155326429+asif-raza01@users.noreply.github.com>
2026-03-08 23:06:26 +0530

    Update  memory address explanation

--- memoryManagement.py
+++ memoryManagement.py
@@ -6,4 +6,4 @@ print(id(b)) #This will return the same memory address as id(a) because small in
 c=1000000000  
 print(id(c)) #This will likely return a different memory address than id(a) and id(b) because larger integers are not cached by Python and may be stored in different locations in memory.
 d=1000000000 
-print(id(d)) #This will likely return the same memory address as id(c) because Python may optimize memory usage by reusing the same object for identical immutable values, even if they are larger integers. However, this behavior can vary based on the implementation and version of Python being used.
+print(id(d)) #Here This will likely return the same memory address as id(c) because Python may optimize memory usage by reusing the same object for identical immutable values, even if they are larger integers. However, this behavior can vary based on the implementation and version of Python being used.
